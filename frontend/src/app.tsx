9532fd9143d2b0bdc7fbfca91e25b47e9dc85ea6
AntonKochurka <antonkotchurka@gmail.com>
2025-02-13 17:50:08 +0200

    added a autogenerated routing system

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,6 +1,22 @@
+import { Provider } from "react-redux";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import store from "./core/redux";
+
+import { routeTree } from './routeTree.gen'
+
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
 const App = () => {
   return (
-    <div>App</div>
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
   )
 }
 
